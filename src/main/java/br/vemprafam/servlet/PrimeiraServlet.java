9571ae9a19b786f9c4a64710f6c830577aec71d3
052787 <052787@SPFAM2LAB142-01.famacad.com.br>
2023-03-14 10:40:08 -0300

    Aula 14/3

---
+++ src/main/java/br/vemprafam/servlet/PrimeiraServlet.java
@@ -0,0 +1,43 @@
+package br.vemprafam.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class PrimeiraServlet
+ */
+@WebServlet("/PrimeiraServlet")
+public class PrimeiraServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public PrimeiraServlet() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("entrou na servlet");
+		PrintWriter out = response.getWriter();
+		out.println("teste da 1a. Servlet");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
---
+++ src/main/java/br/vemprafam/servlet/ServletLista.java
@@ -0,0 +1,82 @@
+package br.vemprafam.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.text.NumberFormat;
+import java.text.SimpleDateFormat;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import br.vemprafam.dao.DaoProduto;
+import br.vemprafam.pojo.Produto;
+
+/**
+ * Servlet implementation class ServletLista
+ */
+@WebServlet("/lista")
+public class ServletLista extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ServletLista() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		DaoProduto dao = new DaoProduto();
+		List<Produto> lista = dao.getLista();
+		PrintWriter out = response.getWriter();
+		out.println("<!DOCTYPE html>");
+		out.println("<html>");
+		out.println("<head>\r\n"
+				+ "<meta charset=\"ISO-8859-1\">\r\n"
+				+ "<title>Lista</title>\r\n"
+				+ "</head>");
+		out.println("<body>");
+		out.println("<table border='1'>");
+		out.println("<tr>\r\n"
+				+ "	<th>código</th>\r\n"
+				+ "	<th>descrição</th>\r\n"
+				+ "	<th>quantidade</th>\r\n"
+				+ "	<th>preço</th>\r\n"
+				+ "	<th>data da compra</th>\r\n"
+				+ "</tr>");
+		SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+		for( Produto produto: lista) {
+			out.println("<tr>");
+			out.println("<td>"+produto.getCodigo()+"</td>");
+			out.println("<td>"+produto.getDescricao()+"</td>");
+			out.println("<td>"+produto.getQuantidade()+"</td>");
+			out.println("<td>"+
+			NumberFormat.getCurrencyInstance().format( produto.getPreco())
+			            +"</td>");
+			out.println("<td>"+
+			            dateFormat.format( produto.getDataCompra())
+			            +"</td>");
+			out.println("</tr>");
+		}
+		out.println("</table>");
+		out.println("</body>");
+		out.println("</html>");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
